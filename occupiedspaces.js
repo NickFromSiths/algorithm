d2431e82ebe911c60299759094388577bc67accc
Nick <nmalyarchuk07@gmail.com>
2024-10-23 13:58:50 -0400

    .

--- occupiedspaces.js
+++ occupiedspaces.js
@@ -1,6 +1,36 @@
-let n = 0;
-let y = [];
-let t = [];
+// let n = 5;
+// let y = ["c","c",".",".","c"];
+// let t = [".","c","c",".","."];
 
-const c = 1;
-const o = 0;
+// const c = 1
+// const . = 1
+
+function occupiedParking(n, yesterday, today) {
+  let count = 0;
+  for (let i = 0; i < n; i++) {
+    if (yesterday[i] === "C" && today[i] === "C") {
+      count++;
+    }
+  }
+  return count;
+}
+
+console.log(occupiedParking(5, "CC..C", ".CC.."));
+
+123;
+
+//decimal to binary
+//
+//subtract bases of 2 from decimal until you get 
+
+
+
+function decToBinary(){
+    let x = 0;
+    for(x < 511) {
+        if(y = 0){}
+    }
+    else {
+        console.log("Too big")
+    }
+}
